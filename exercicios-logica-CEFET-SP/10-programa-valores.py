096778b522529201415616e1885fdd666d601da9
Ayrton Araújo <95201381+ayrtonaraujo@users.noreply.github.com>
2022-01-06 20:05:30 -0400

    1

---
+++ exercicios-logica-CEFET-SP/10-programa-valores.py
@@ -0,0 +1,13 @@
+#10) Elaborar um programa para receber valores,
+# via  teclado, nas  variáveis "a" e "b". Após
+# isto, o programa, utilizando-se de uma 3a. variável
+# "c", deverá trocar o conteúdo das variáveis "a" e "b".
+
+a = input('A:')
+b = input('B:')
+c = input('C:')
+
+a = c
+b = c
+
+print (a,'e', b)
